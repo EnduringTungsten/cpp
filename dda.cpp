0982b7c16de5e1e80431836ddea149429ff51ac8
EnduringTungsten <128391593+EnduringTungsten@users.noreply.github.com>
2024-12-12 14:00:40 +0530

    Add files via upload

---
+++ dda.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[3][3],i,j;
+    cout<<"Enter numbers of dda\n";
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    cout<<"Elements of dda are\n";
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            cout<<a[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
